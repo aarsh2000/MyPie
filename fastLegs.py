d1158f209d67bfb6dbfd3ac38cfc98618d64b0e5
Aarsh Patel <32023476+aarsh2000@users.noreply.github.com>
2018-10-05 13:32:35 -0400

    Update fastLegs.py

--- fastLegs.py
+++ fastLegs.py
@@ -1,4 +1,5 @@
 import pygame
+from random import random
 pygame.init()
 
 win_width = 1000
@@ -10,25 +11,67 @@ width = 50
 height = 100
 x = 60
 y = win_height - height - 100
-speed = 3
+speed = 5
 
-y_constant =  y
+y_constant = y
 
-jumpSpeed = 8
+jumpSpeed = 100
 jump = False
 
 #Keep delayLimit even number
-delayLimit = 26
+delayLimit = 28
 delay = -delayLimit
 
-pygame.display.set_caption("Fast Legs")
 
+pygame.display.set_caption("Fast Legs")
+radius= int(height / 3)
+cx=win_width
+cy=y_constant + height - radius
+
+score = 0
+speed_base = 3
+speed_multiplier = 2
+class Circle:
+    def __init__(self,radius, dx, x, y):
+        self.radius = radius
+        self.dx = dx
+        self.x = x
+        self.y = y
+    def draw_circle(self):
+        pygame.draw.circle(win, (0, 0, 0), (self.x, self.y), self.radius, 0)
+        self.reset_circle()
+        self.x -= self.dx
+    def add_circle(self):
+        Circles.append(self)
+    def reset_circle(self):
+        if self.x < 0:
+            global score
+            global speed_base
+            score += 1
+            self.x = cx
+            self.dx = int(random()*speed_multiplier+speed_base)
+            if score%12 == 0 and score<=48:
+                speed = int(random() * 9 + 1)
+                temp = Circle(radius, speed, cx, cy)
+                temp.add_circle()
+            if score%48==0:
+                speed_base +=2
+
+
+
+Circles = []
+
+for i in range(1):
+    speed = int(random()*speed_multiplier+speed_base)
+    temp = Circle(radius, speed, cx, cy)
+    temp.add_circle()
 
 
 run = True
 
 while run:
-    pygame.time.delay(50)
+    pygame.time.delay(10)
+
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -36,9 +79,9 @@ while run:
 
     key = pygame.key.get_pressed()
 
-    if key[pygame.K_LEFT]:
+    if (key[pygame.K_LEFT] or key[pygame.K_a]):
             x -= speed
-    if key[pygame.K_RIGHT]:
+    if (key[pygame.K_RIGHT] or key[pygame.K_d]):
             x += speed
 
 
@@ -48,14 +91,14 @@ while run:
 
     if jump:
         if delay < 0:
-            y -= (delay*delay)/50
+            y -= (delay*delay)/80
             delay += 1
 
         elif delay == 0:
             delay += 1
 
         elif delay <= delayLimit:
-            y += (delay*delay)/50
+            y += (delay*delay)/80
             delay += 1
 
         elif delay == delayLimit+1:
@@ -64,12 +107,23 @@ while run:
 
 
     win.fill((0, 0, 0))
-    # Background
+    # Background ground
     pygame.draw.rect(win, (2, 204, 18), (0, y_constant + height, win_width, win_height))
+    #sky
     pygame.draw.rect(win, (46, 132, 244), (0, 0, win_width, y_constant + height))
+    # obstacle
+    pygame.draw.polygon(win, (255, 255, 0), ((0, 225), (25, 160), (400, 160), (425, 275)))
 
-    pygame.draw.rect(win, (255, 255, 0), (x, y, width, height))
+
+    for i in range(len(Circles)):
+        Circles[i].draw_circle()
+
+
+    #Rectangle character
+    pygame.draw.rect(win, (255, 0, 0), (x, y, width, height))
     pygame.display.update()
+
+
 
 pygame.quit()
 
